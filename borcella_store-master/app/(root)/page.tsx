448870de38118c4e80976df74530189c6d4f317c
VACHAN SHETTY <vachan316@gmail.com>
2024-05-07 15:05:23 +0530

    first

---
+++ borcella_store-master/app/(root)/page.tsx
@@ -0,0 +1,22 @@
+import Collections from "@/components/Collections";
+import ProductList from "@/components/ProductList";
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Image
+        src="/bannerown.png"
+        alt="banner"
+        width={2000}
+        height={1000}
+        className="w-screen"
+      />
+      <Collections />
+      <ProductList />
+    </>
+  );
+}
+
+export const dynamic = "force-dynamic";
